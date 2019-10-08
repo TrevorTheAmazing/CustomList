e7bcdc88dcb0418a54b614e06ad1468967aaa722
trevor clements <trevortheamazing@gmail.com>
2019-10-08 12:02:46 -0500

    pre-lunch push.

--- Sandbox-C#/Sandbox/CustomList.cs
+++ Sandbox-C#/Sandbox/CustomList.cs
@@ -8,16 +8,56 @@ namespace Sandbox
 {
     public class CustomList<T>
     {
+        //memb var
+        //array of items
         private T[] items;
+        private T[] tempItems;
+        private int capacity;
 
         public CustomList()
         {
-            items = new T[4];
+            capacity = 1;
+            items = new T[capacity];
+        }
+
+        private void IncreaseCapacity()
+        {
+            tempItems = new T[capacity + 1];
+            
+            for (int i = 0; i<items.Length; i++)
+            {
+                tempItems[i] = items[i];
+            }
+
+            items = tempItems;
+
+        }
+
+        public void Add(T itemToAdd, int index)
+        {
+            //assign tempCap to array.capacity
+            //reassign items to a new array(tempCap+1)
+
+            
+             //increase capacity
+             IncreaseCapacity();
+
+             if (index != (capacity-1))
+             {
+                 items.Prepend(itemToAdd);
+             }
+             else
+             {
+                 items.Append(itemToAdd);
+             }
+
         }
 
         public void Add(T itemToAdd)
         {
-
+            //If an index is not specified, add it to the end of the array.
+            Add(itemToAdd, capacity);
         }
+
     }
 }
